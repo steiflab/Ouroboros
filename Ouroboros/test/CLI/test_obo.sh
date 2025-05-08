8a45ab437d8634f9ec381da621361991e60a2871
Haley MacDonald <hmacdonald@bcgsc.ca>
2025-05-08 14:15:04 -0700

    Remove stray 'Ouroboros' directory from Git tracking

--- Ouroboros/test/CLI/test_obo.sh
+++
@@ -1,5 +0,0 @@
-python -m Ouroboros_pypi.Ouroboros.main \
-    --data /projects/steiflab/research/hmacdonald/total_RNA/data/hayashi_mesc/RamDA_scratch/processed/mESC_processed.h5ad  \
-    --data_type h5ad \
-    --species mouse \
-    --outdir /projects/steiflab/research/hmacdonald/total_RNA/Ouroboros_pypi/Ouroboros/test/CLI
