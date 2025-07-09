45a5d9fc4868fc0172cacc72322987ab5c062d1c
Glenn Chang <glchang@bcgsc.ca>
2025-07-09 11:51:18 -0700

    Output QC and find new threshold

--- ouroboros/ouroboros_functions.py
+++ ouroboros/ouroboros_functions.py
@@ -12,8 +12,8 @@ from sklearn.neighbors import KNeighborsClassifier
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 from sklearn.decomposition import PCA
-from scipy.signal import argrelextrema
 
+from kneed import KneeLocator
 from scipy.interpolate import UnivariateSpline
 import tensorflow as tf
 import random
@@ -266,7 +266,7 @@ def ouroboros_retrain(test_adata, seed):
     train_df = z_mean_df.merge(train_meta, how = 'left', left_index = True, right_index = True)
     
     
-    return model, train_df, in_order_feature_set
+    return model, train_df, in_order_feature_set, trainer
 
 
 
@@ -1739,38 +1739,68 @@ def plot_robinson_projection(
     plt.show()
 
 
-def find_threshold(model, new_feature_set, z_df, ref_embed, outdir):
+def qc_and_threshold(model, trainer, z_df, new_feature_set, ref_embed, outdir, seed, debug = True):
     """
     Depreciated function; Attepts to find threshold for quiscence and senescence. 
     """
+
+    ## Process Wetchner dataset
     discrete = ad.read_h5ad("/projects/steiflab/scratch/hmacdonald/total_RNA_scratch/wechter_scratch/starsolo_counts/h5ads/discrete.h5ad")
-    bdata = discrete[:, discrete.var_names.isin(new_feature_set)].copy()
-    matrix = bdata.layers['raw_counts'].toarray()
-
-    new_batch = np.full(matrix.shape[0], 2)
-
-    # Project new data
-    z_mean = model.encode(matrix, new_batch)
-    z_mean_df = pd.DataFrame(z_mean, index=discrete.obs_names, columns=["dim1", "dim2", 'dim3'])
-    
+    discrete.X = discrete.layers['raw_counts'].toarray()
+    z_mean_df = embed_in_retrained_sphere(discrete, model, new_feature_set)
     z_mean_df = KNN_predict(ref_embed, z_mean_df)    
     z_mean_df = calculate_cell_cycle_pseudotime(z_mean_df, ref_embed,  phase_category = 'KNN_phase')
-    pseud = dormancy_depth(z_mean_df, ref_embed, retrained = False)
+    pseud, _ = dormancy_depth(z_mean_df, ref_embed, retrained = True)
     z_mean_df = z_mean_df.merge(pseud, how = 'left', left_index = True, right_index = True)
     z_mean_df = z_mean_df.merge(discrete.obs[['rep', 'treatment']], left_index=True, right_index=True)
     
-    
-    # Bin data
-    num_bins = 30
+    if debug:
+        plt.hist(z_mean_df['dormancy_depth'])
+        plt.savefig(f"{outdir}/wetchner.png")
+        plt.close()
 
-    discrete_filtered = z_mean_df.dropna(subset=["dormancy_depth"]).copy()
+    z_mean_df['pseudotime'] = np.where(z_mean_df['dormancy_depth'].isna(), z_mean_df['cell_cycle_pseudotime'], z_mean_df['dormancy_depth'])
+
+    ## Process training dataset
+    training_embed = ref_embed.copy()
+    cc_df = calculate_cell_cycle_pseudotime(training_embed, ref_embed,  phase_category = 'phase')
+    cc_df = cc_df[['cell_cycle_pseudotime']]
+    training_embed = training_embed.merge(cc_df, how = 'left', left_index = True, right_index = True)
+    pseud, ref_pseud = dormancy_depth(training_embed, ref_embed, retrained = True)
+    training_embed = training_embed.merge(pseud, how = 'left', left_index = True, right_index = True)
+    
+    training_embed['pseudotime'] = np.where(training_embed['dormancy_depth'].isna(), training_embed['cell_cycle_pseudotime'], training_embed['dormancy_depth'])
+
+
+
+    ### Find Threshold
+    threshold = find_threshold(z_mean_df)
+    z_df = z_df.copy()
+
+    z_df['G0_classification'] = np.where(
+        z_df['dormancy_depth'] > threshold, 'quiescence',
+        np.where(
+            z_df['dormancy_depth'] < threshold, 'senescence',
+            np.nan
+        )
+    ) 
+
+    ### QC
+    qc = quality_control(trainer, z_mean_df, training_embed, new_feature_set, threshold, seed, outdir)
+    qc.to_csv(f"{outdir}/qc.csv", sep=',')
+
+    return z_df
+
+def find_threshold(wetchner_df):
+    num_bins = 30
+    senescence_df = wetchner_df[wetchner_df['treatment'].isin(['IR-induced senescence (10 Gy)', 'Replicative senescence (PDL 57)', 'Etoposide-induced senescent (50 microM)'])]
+
+    discrete_filtered = senescence_df.dropna(subset=["dormancy_depth"]).copy()
     min_val = discrete_filtered["dormancy_depth"].min()
     max_val = discrete_filtered["dormancy_depth"].max()
     bin_edges = np.linspace(min_val, max_val, num_bins + 1)
     discrete_filtered["pseudotime_bin"] = pd.cut(discrete_filtered["dormancy_depth"], bins=bin_edges, include_lowest=True)
-    assert discrete_filtered["pseudotime_bin"].isna().sum() == 0
 
-    # 
     discrete_filtered = discrete_filtered[discrete_filtered['treatment'].isin(['IR-induced senescence (10 Gy)', 'Replicative senescence (PDL 57)', 'Etoposide-induced senescent (50 microM)'])]
     bin_counts = discrete_filtered.groupby(["pseudotime_bin"]).size()
     bin_proportions = bin_counts.div(bin_counts.sum(axis=0))
@@ -1784,42 +1814,75 @@ def find_threshold(model, new_feature_set, z_df, ref_embed, outdir):
     x_dense = np.linspace(x.min(), x.max(), 500)
     y_smooth = spline(x_dense)
 
-   # Find global maximum
-    peak_idx_max = np.argmax(y_smooth)
+    kl = KneeLocator(x_dense, y_smooth, curve="convex", direction="decreasing", online=True)
+    knee = kl.knee
 
-    # Region after the peak
-    y_tail = y_smooth[peak_idx_max:]
-    x_tail = x_dense[peak_idx_max:]
+    return knee
+    
 
-    # Try to find local minima
-    local_min_indices = argrelextrema(y_tail, np.less)[0]
-    if len(local_min_indices) > 0:
-        # Use first local minimum after peak
-        min_idx = local_min_indices[0]
-    else:
-        # No local min: fallback to absolute minimum in tail
-        min_idx = np.argmin(y_tail)
 
-    knee = (x_dense[peak_idx_max] + x_dense[peak_idx_max + min_idx]) / 2
+def quality_control(trainer, wetchner_df, training_df, new_feature_set, threshold, seed, outdir):
+    # Recall using Wechner dataset
+    wetchner_df['pseudotime'] = np.where(wetchner_df['dormancy_depth'].isna(), wetchner_df['cell_cycle_pseudotime'], wetchner_df['dormancy_depth'])
+    senescence_df = wetchner_df[wetchner_df['treatment'].isin(['IR-induced senescence (10 Gy)', 'Replicative senescence (PDL 57)', 'Etoposide-induced senescent (50 microM)'])]
 
-    plt.plot(x_dense, y_smooth)
-    plt.axvline(knee)
-    plt.title(f'Wechter senescence dataset\nThreshold: {knee}')
-    plt.xlabel("Dormancy Depth")
-    plt.ylabel("Proportion of cells")
-    plt.savefig(f"{outdir}/dormancy_depth_threshold.png")
-    plt.close()
+    senescene = senescence_df[senescence_df['pseudotime'] < -0.6].shape[0]
+    true_senescence = senescence_df.shape[0]
+    senescence_recall = senescene / true_senescence
 
-    z_df = z_df.copy()
-    z_df['G0_classification'] = np.where(
-        z_df['dormancy_depth'] > knee, 'quiescence',
-        np.where(
-            z_df['dormancy_depth'] < knee, 'senescence',
-            np.nan
-        )
-    ) 
-    return z_df
+    # Model log likelihood
+    log_likihood = trainer.status['log_likelihood'][-1] 
+    kl_divergenet = trainer.status['kl_divergence'][-1]
 
+    # Missing genes 
+    feature_set = pd.read_csv(DATA_DIR / 'SHAP_feature_set.csv')
+    feature_set = feature_set.feature_set.tolist()
+    missing_gene = len(set(feature_set) - set(new_feature_set))
+    proportion_missing_gene = missing_gene / len(feature_set)
+
+    # SHAP score loss
+    shap_score = pd.read_csv("/projects/steiflab/research/hmacdonald/total_RNA/Ouroboros_paper/model/feature_selection/output/mean_shap_values.csv")
+
+    shap_loss = {}
+    for cell_cycle in ['G0', 'G1', 'G1-G0 transition', 'G2M', 'S']:
+        curr_cell_cycle = shap_score[shap_score['Class'] == cell_cycle]
+        curr_cell_cycle = curr_cell_cycle[curr_cell_cycle['Feature'].isin(feature_set)]
+        total_shap_score = curr_cell_cycle['AbsoluteSHAPValue'].sum()
+        shap_score_loss = curr_cell_cycle[~curr_cell_cycle['Feature'].isin(new_feature_set)]['AbsoluteSHAPValue'].sum()
+
+        prop_shap_loss = shap_score_loss/total_shap_score
+        shap_loss[f'{cell_cycle}_shap_loss'] = [prop_shap_loss]
+
+
+    # Scenscenece threshold Diff
+    threshold_diff = abs(-0.6 - threshold)
+
+    # Scenscence gene expression score
+    wetchner_training = pd.concat([wetchner_df, training_df], axis=0)
+    senescence_score = pd.read_csv('/projects/steiflab/scratch/glchang/Ouroboros_paper/senescence.csv')
+    wetchner_training = wetchner_training.merge(senescence_score, right_on="cell_id", left_index = True)
+
+    wetchner_training = wetchner_training[~wetchner_training['dormancy_depth'].isna()]
+    
+    senmayo_corr = wetchner_training['dormancy_depth'].corr(wetchner_training['senmayo'], method='spearman')
+    hernandez_segura_corr = wetchner_training['dormancy_depth'].corr(wetchner_training['core_up_sen_genes'], method='spearman')
+
+   
+
+
+    qc_df = pd.DataFrame({
+        'seed': [seed],
+        'wetchner_senescence_recall': [senescence_recall],
+        'log_likihood': [log_likihood],
+        'kl_divergenet': [kl_divergenet],
+        'missing_gene': [missing_gene], 
+        'proportion_missing_gene': [proportion_missing_gene],
+        'threshold_diff': [threshold_diff],
+        'senmayo_corr': [senmayo_corr],
+        'hernandez_segura_corr': [hernandez_segura_corr],
+        **shap_loss
+    })
+    return qc_df
 
 def set_seed(seed=0):
     random.seed(seed)
