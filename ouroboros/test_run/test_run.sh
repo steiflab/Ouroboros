aaeed0a925046e012e2571c8d3014568d8d3aa39
Haley Beckmann <hmacdonald@bcgsc.ca>
2026-09-22 18:05:14 -0700

    Remove dev/test files

--- ouroboros/test_run/test_run.sh
+++
@@ -1,5 +0,0 @@
-ouroboros \
-    --data /projects/steiflab/research/hmacdonald/total_RNA/data/neural_diff/neural_diff_scratch/processed_data/neurogenesis_wt_preprocessed.h5ad \
-    --data_type h5ad \
-    --species human \
-    --outdir .
